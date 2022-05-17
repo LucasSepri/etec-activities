35b872deb4b0707c683460c7c71023c392c849f0
Lucas Sepriano <lucassepriano@gmail.com>
2022-05-16 21:36:04 -0300

    Add files via upload

---
+++ PW2/9.2/index.php
@@ -0,0 +1,9 @@
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+<?php
+    $nome = "Lucas Sepriano";
+    $email = "lucassepriano@gmail.com";
+
+    echo "\"Olá $nome, Bem Vindo! </br> Seu email é $email\"";
+    echo '</br></br>"' . "Olá $nome, Bem Vindo! </br> Seu email é $email" . '"';
+?>
